372f1670bfc56ea7de7076fcb136f2a79d4323de
邱浩楠 <Arthur>
2019-09-19 01:07:54 +0800

    1

--- data/segmentationadv_dataset.py
+++ data/segmentationadv_dataset.py
@@ -139,9 +139,9 @@ class SegmentationAdvDataset(BaseDataset):
         outputs['target_bbox'] = torch.from_numpy(target_bbox)
         outputs['output_bbox'] = torch.from_numpy(output_bbox)
         outputs['mask_in'] = mask_in # (1x1xHxW)
-        mask_target, _object_target, _context_target = get_masked_image(
-            outputs['label'], target_bbox)
-        outputs['mask_target'] = mask_target  # (1x1xHxW)
+        # mask_target, _object_target, _context_target = get_masked_image(
+        #     outputs['label'], target_bbox)
+        # outputs['mask_target'] = mask_target  # (1x1xHxW)
         outputs['mask_object_in'] = mask_object_in # (1xCxHxW)
         outputs['mask_context_in'] = mask_context_in # (1xCxHxW)
         outputs['mask_out'] = mask_out # (1x1xHxW)
@@ -159,13 +159,13 @@ class SegmentationAdvDataset(BaseDataset):
                                     self.class_of_interest, self.config, bbox=inst_info1["object"], target_box = inst_info1["target"],
                                     random_crop=self.opt.random_crop)
       outputs = self.preprocess_inputs(raw_inputs, params)
-      # if inst_info1["target"]['inst_ids'] is None:
-      #     mask_target = torch.where(outputs['inst'] == inst_info1["target"]['inst_id'],torch.full_like(outputs['inst'], 1),torch.full_like(outputs['inst'], 0))
-      # else:
-      #     mask_target1 = torch.where(outputs['inst'] == inst_info1["target"]['inst_ids'][0],torch.full_like(outputs['inst'], 1),torch.full_like(outputs['inst'], 0))
-      #     mask_target2 = torch.where(outputs['inst'] == inst_info1["target"]['inst_ids'][1],torch.full_like(outputs['inst'], 1), torch.full_like(outputs['inst'], 0))
-      #     mask_target = mask_target1 | mask_target2
-      # outputs['mask_target'] = mask_target.float()
+      if inst_info1["target"]['inst_ids'] is None:
+          mask_target = torch.where(outputs['inst'] == inst_info1["target"]['inst_id'],torch.full_like(outputs['inst'], 1),torch.full_like(outputs['inst'], 0))
+      else:
+          mask_target1 = torch.where(outputs['inst'] == inst_info1["target"]['inst_ids'][0],torch.full_like(outputs['inst'], 1),torch.full_like(outputs['inst'], 0))
+          mask_target2 = torch.where(outputs['inst'] == inst_info1["target"]['inst_ids'][1],torch.full_like(outputs['inst'], 1), torch.full_like(outputs['inst'], 0))
+          mask_target = mask_target1 | mask_target2
+      outputs['mask_target'] = mask_target.float()
       if self.config['preprocess_option'] == 'select_region':
           outputs = self.preprocess_cropping(raw_inputs, outputs, params)
       return outputs
