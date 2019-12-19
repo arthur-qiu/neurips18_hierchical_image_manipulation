571d5e015cf962692c64b474c85a4ef5d84f9a68
邱浩楠 <Arthur>
2019-12-19 22:26:33 +0800

    check

--- scripts/check_inst.py
+++ scripts/check_inst.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import numpy as np
-a = Image.open('/Users/qiuhaonan/Downloads/gtFine_trainvaltest/new_candidates/p93/frankfurt_000000_019607_gtFine_instanceIds3.png')
+a = Image.open('/Users/qiuhaonan/Downloads/gtFine_trainvaltest/p19/munster_000014_000019_gtFine_instanceIds3.png')
 b = np.array(a, dtype=np.int32)
-c = Image.open('/Users/qiuhaonan/Downloads/gtFine_trainvaltest/new_candidates/p93/frankfurt_000000_019607_gtFine_instanceIds.png')
+c = Image.open('/Users/qiuhaonan/Downloads/gtFine_trainvaltest/p19/munster_000014_000019_gtFine_instanceIds.png')
 d = np.array(c, dtype=np.int32)
-e = b[422,330]
+e = b[474,1020]
 print(e)
-f = b[414,153]
+f = b[457,1190]
 # f = b[539,747]
 print(f)
 
@@ -43,24 +43,24 @@ ymin, ymax, xmin, xmax = \
                     ys.min(), ys.max(), xs.min(), xs.max()
 
 print(xmin.item(), ymin.item(), xmax.item(), ymax.item())
-
-ys,xs = np.where(b==f)
-for i in range(len(ys)):
-    if b[ys[i],xs[i]] != f:
-        print(ys[i],xs[i])
-    # b[ys[i], xs[i]] = b[ys[i], xs[i]] + 50
-    if d[ys[i],xs[i]] == f:
-        b[ys[i], xs[i]] = b[ys[i], xs[i]] + 50
-    # if xs[i] > 990:
-    #     print(ys[i],xs[i])
-
-ys,xs = np.where(b==f+50)
-ymin, ymax, xmin, xmax = \
-                    ys.min(), ys.max(), xs.min(), xs.max()
-
-print(xmin.item(), ',', ymin.item(), ',', xmax.item(), ',', ymax.item())
+#
+# ys,xs = np.where(b==f)
+# for i in range(len(ys)):
+#     if b[ys[i],xs[i]] != f:
+#         print(ys[i],xs[i])
+#     # b[ys[i], xs[i]] = b[ys[i], xs[i]] + 50
+#     if d[ys[i],xs[i]] != f:
+#         b[ys[i], xs[i]] = b[ys[i], xs[i]] + 50
+#     # if xs[i] > 990:
+#     #     print(ys[i],xs[i])
+#
+# ys,xs = np.where(b==f+50)
+# ymin, ymax, xmin, xmax = \
+#                     ys.min(), ys.max(), xs.min(), xs.max()
+#
+# print(xmin.item(), ',', ymin.item(), ',', xmax.item(), ',', ymax.item())
 
 
 
 d = Image.fromarray(b)
-d.save('/Users/qiuhaonan/Downloads/gtFine_trainvaltest/new_candidates/p93/frankfurt_000000_019607_gtFine_instanceIds1.png')
+d.save('/Users/qiuhaonan/Downloads/gtFine_trainvaltest/p19/munster_000014_000019_gtFine_instanceIds1.png')
