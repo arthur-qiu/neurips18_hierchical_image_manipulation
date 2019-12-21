5cb3e8f4c174fff94ace6155ffa37e6ca65ad1f8
邱浩楠 <Arthur>
2019-12-21 23:53:48 +0800

    add detect

--- models/pix2pixHD_detectAdv_model.py
+++ models/pix2pixHD_detectAdv_model.py
@@ -475,7 +475,8 @@ class Pix2PixHDModel_detectAdv(BaseModel):
             alpha_in = torch.clamp(alpha, 0.6, 1.0)
             semantic_image = self.netG.g_out((fake_feature_const * (1-alpha_in) + fake_feature1_const * alpha_in), ctx_feats, cond_image, mask_in)
             x_hat = (semantic_image + 1.0) / 2
-            # x_hat, _ = pad_to_square(x_hat, 0)
+            x_hat, _ = pad_to_square(x_hat, 0)
+            x_hat.requires_grad = True
             out = self.netS(x_hat)[0]
             cfs = nn.functional.sigmoid(out[:, 4]).cuda()
             mask = (cfs >= conf_threshold).type(torch.FloatTensor).cuda()
