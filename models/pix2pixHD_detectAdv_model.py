7fa5afc554f124d1ba46d5f8f485ef58c34f381a
邱浩楠 <Arthur>
2019-12-22 17:34:16 +0800

    add detect

--- models/pix2pixHD_detectAdv_model.py
+++ models/pix2pixHD_detectAdv_model.py
@@ -487,9 +487,9 @@ class Pix2PixHDModel_detectAdv(BaseModel):
             cfs = nn.functional.sigmoid(out[:, 4]).cuda()
 
             xywh = out[:, :4].clone().detach().cuda()
-            xyxy = xywh2xyxy(xywh) 
+            xyxy = xywh2xyxy(xywh)
 
-            mask = (cfs >= clf_threshold & xyxy[:,0] >= target_x_min & xyxy[:,1] >= target_y_min & xyxy[:,2] <= target_x_max & xyxy[:,3] <= target_y_max).type(torch.FloatTensor).cuda()
+            mask = ((cfs >= clf_threshold & xyxy[:,0] >= target_x_min) & (xyxy[:,1] >= target_y_min) & (xyxy[:,2] <= target_x_max) & (xyxy[:,3] <= target_y_max)).type(torch.FloatTensor).cuda()
             num_pred = torch.numel(cfs)
             removed = torch.sum((cfs < clf_threshold).type(torch.FloatTensor)).data.cpu().numpy()
 
